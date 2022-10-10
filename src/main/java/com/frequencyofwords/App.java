b7b9e1c1e413b937f457818016f7d22a03077364
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-10-10 15:20:22 +0530

    Added Project Files and Dependencies

---
+++ src/main/java/com/frequencyofwords/App.java
@@ -0,0 +1,13 @@
+package com.frequencyofwords;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App 
+{
+	private static final Logger logger = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        System.out.println( "Frequency of Words using HashTable!" );
+    }
+}
