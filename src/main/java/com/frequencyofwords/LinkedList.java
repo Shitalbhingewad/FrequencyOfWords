408821872a363bac6d0d6956f411b7d25512eabd
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-10-10 16:06:10 +0530

    Finding index using hashcode

--- src/main/java/com/frequencyofwords/LinkedList.java
+++ src/main/java/com/frequencyofwords/LinkedList.java
@@ -1,5 +1,84 @@
 package com.frequencyofwords;
 
 public class LinkedList<K, V> {
+    MyMapNode<K, V> head;
 
+    public void add(K key, V value) {
+        MyMapNode<K, V> newNode = new MyMapNode<K, V>(key, value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            MyMapNode<K, V> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void replace(K key, V value) {
+        MyMapNode<K, V> temp = head;
+        while (temp != null) {
+            if (temp.key.equals(key)) {
+                temp.value = value;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public boolean containsKey(String key) {
+        MyMapNode<K, V> temp = head;
+        while (temp != null) {
+            if (temp.key.equals(key)) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+
+    }
+
+    public int get(String word) {
+        MyMapNode<K, V> temp = head;
+        int count = 0;
+        while (temp != null) {
+            if (temp.key.equals(word)) {
+                count++;
+            }
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public MyMapNode<K, V> search(K searchData) {
+        MyMapNode<K, V> temp = head;
+        while (temp != null) {
+            if (temp.key.equals(searchData))
+                return temp;
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public void append(MyMapNode<K, V> node) {
+
+        if (head == null) {
+            head = node;
+
+        } else {
+            MyMapNode<K, V> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    public void print() {
+        MyMapNode<K, V> temp = head;
+        while (temp != null) {
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
 }
